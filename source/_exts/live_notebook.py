11e665da4dbbce3dd35a584f1f9b91b8168e12b2
Mark Liffiton <liffiton@gmail.com>
2020-12-04 22:14:33 -0600

    Drop jhub mention -- no longer used for this class.

--- source/_exts/live_notebook.py
+++ source/_exts/live_notebook.py
@@ -2,16 +2,15 @@ from docutils.parsers.rst.directives.admonitions import Note
 from docutils.statemachine import ViewList
 
 _TEMPLATE = '''
-This is a static copy of a Jupyter notebook.  You can access a live
-version of it, allowing you to modify and execute the code, in one of two ways:
+This is a static copy of a Jupyter notebook.
 
-- `Jhub
-  <https://jhub.iwu.edu/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2FCS-DS-125%2F125book&branch=master&urlPath=lab/tree/125book/{{NOTEBOOK_PATH}}>`_
-  (for students in IWU's CS/DS course)
-- `Binder
-  <https://mybinder.org/v2/gh/CS-DS-125/125book/master?urlpath=lab/tree/{{NOTEBOOK_PATH}}&filepath={{NOTEBOOK_PATH}}>`_
-  (for anyone else)
+You can access a live version allowing you to modify and execute the code using
+`Binder <https://mybinder.org/v2/gh/CS-DS-125/125book/master?urlpath=lab/tree/{{NOTEBOOK_PATH}}&filepath={{NOTEBOOK_PATH}}>`_.
 '''
+
+#- `Jhub
+#  <https://jhub.iwu.edu/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2FCS-DS-125%2F125book&branch=master&urlPath=lab/tree/125book/{{NOTEBOOK_PATH}}>`_
+#  (for students in IWU's CS/DS course)
 
 
 def setup(app):
